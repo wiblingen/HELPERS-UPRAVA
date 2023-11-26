6c65b27589f58dbf4bb877725f2518d0a4a93803
Chipster <go@w.ay>
2023-11-26 17:20:00 -0600

    update

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -11,6 +11,10 @@ uuidStr=$(egrep 'UUID|ModemType|ModemMode|ControllerType' /etc/pistar-release | 
 hwDeetz=$( /usr/local/sbin/platformDetect.sh )
 uaStr="Server-Side Exec: WPSD-BG-Bootstrap-Task Ver.# ${dashVer} Call:${CALL} UUID:${uuidStr} [${hwDeetz}] [${osName}]"
 
+# func to fix stuck updates
+if [ ! -f '/usr/local/sbin/wpsd-update' ]; then
+    curl -s -A "sbin-phix $uaStr"-Ls https://repo.w0chp.net/Chipster/W0CHPist/raw/branch/master/reset-wpsd-sbin | sudo bash > /dev/null 2<&1
+fi
 
 # func to fixup sbin and stash any upnp service changes:
 gitFolder="/usr/local/sbin"
