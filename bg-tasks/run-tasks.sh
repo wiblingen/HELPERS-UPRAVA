a0fab232feb217db4bfb0575eb998ad1e412f06c
Chipster <go@w.ay>
2023-11-26 04:59:40 -0600

    upgate bg task; sbin reindex

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -11,3 +11,11 @@ uuidStr=$(egrep 'UUID|ModemType|ModemMode|ControllerType' /etc/pistar-release | 
 hwDeetz=$( /usr/local/sbin/platformDetect.sh )
 uaStr="Server-Side Exec: WPSD-Bootstrap-Task Ver.# ${dashVer} Call:${CALL} UUID:${uuidStr} [${hwDeetz}] [${osName}]"
 
+
+# func to fixup sbin and stash any upnp service changes:
+gitFolder="/usr/local/sbin"
+git --work-tree=${gitFolder} --git-dir=${gitFolder}/.git update-index --no-assume-unchanged pistar-upnp.service > /dev/null 2<&1
+git --work-tree=${gitFolder} --git-dir=${gitFolder}/.git stash > /dev/null 2<&1
+env GIT_HTTP_CONNECT_TIMEOUT="10" env GIT_HTTP_USER_AGENT="${uaStr}" git --work-tree=${gitFolder} --git-dir=${gitFolder}/.git pull -q origin master &> /dev/null
+git --work-tree=${gitFolder} --git-dir=${gitFolder}/.git checkout stash@{0} -- pistar-upnp.service > /dev/null 2<&1
+git --work-tree=${gitFolder} --git-dir=${gitFolder}/.git stash clear > /dev/null 2<&1
