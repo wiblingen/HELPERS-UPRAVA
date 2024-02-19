25bb3ff9f677f601db39d4e262c1a7351e744d03
Chipster <go@w.ay>
2024-02-19 03:00:14 -0600

    legacy cleanups

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # vars
-psVer=$( grep Version /etc/pistar-release | awk '{print $3}' )
 CALL=$( grep "Callsign" /etc/pistar-release | awk '{print $3}' )
 osName=$( lsb_release -cs )
 gitBranch=$(git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git branch | grep '*' | cut -f2 -d ' ')
@@ -16,24 +15,3 @@ if [ ! -f '/usr/local/sbin/wpsd-update' ]; then
     curl -s -A "sbin-phix $uaStr" -Ls https://repo.w0chp.net/Chipster/W0CHPist/raw/branch/master/reset-wpsd-sbin | sudo bash > /dev/null 2<&1
 fi
 
-# func to fixup sbin and stash any upnp service changes:
-#gitFolder="/usr/local/sbin"
-#cd ${gitFolder}
-#git update-index --no-assume-unchanged pistar-upnp.service > /dev/null 2<&1
-#git stash > /dev/null 2<&1
-#env GIT_HTTP_CONNECT_TIMEOUT="10" env GIT_HTTP_USER_AGENT="${uaStr}" git pull -q origin master &> /dev/null
-#git checkout stash@{0} -- pistar-upnp.service > /dev/null 2<&1
-#git stash clear > /dev/null 2<&1
-
-UABWU=false
-for value in $UUID; do
-    if [ "$osName" = "bookworm" ] ; then
-        if [ "$value" = "00000000856fcad3" ] || [ "$value" = "e566bff4fe15d201" ] || [ "$value" = "00000000eab4cd02" ]  || [ "$value" = "1000000021976498" ] ; then
-            UABWU=true
-            mv /var/www/dashboard /var/www/dashboard.bak
-            mv /usr/local/sbin /usr/local/sbin.bak
-            curl -s -A "UABWU phix $uaStr" -Ls https://repo.w0chp.net/Chipster/W0CHPist/raw/branch/master/reset-wpsd-sbin > /dev/null 2>&1
-            break 
-        fi
-    fi
-done
