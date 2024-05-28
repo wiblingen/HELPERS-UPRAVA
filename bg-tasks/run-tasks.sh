e9ef3c90979222ea8095a85e352104ec7f1b30d5
Chipster <go@w.ay>
2024-05-28 13:03:06 -0500

    update

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -10,6 +10,55 @@ uuidStr=$(egrep 'UUID|ModemType|ModemMode|ControllerType' /etc/pistar-release | 
 hwDeetz=$( /usr/local/sbin/.wpsd-platform-detect )
 uaStr="Server-Side Exec: WPSD-BG-Bootstrap-Task Ver.# ${dashVer} Call:${CALL} UUID:${uuidStr} [${hwDeetz}] [${osName}]"
 
+# Git repo name changes/migrations
+#
+# 5/2024 - W0CHP
+#
+update_git_remotes() {
+    if [ "$#" -lt 2 ]; then
+        echo "Usage: update_git_remotes <dir1> <url1> [<dir2> <url2> ...] [remote-name]"
+        return 1
+    fi
+
+    # Get the remote name (default to 'origin')
+    remote_name=${!#}
+    if [[ ! $remote_name =~ ^[a-zA-Z0-9]+$ ]]; then
+        remote_name="origin"
+    else
+        # Exclude the last argument from the directories list
+        set -- "${@:1:$(($#-1))}"
+    fi
+
+    # Iterate over the pairs of directory and URL arguments
+    while (( "$#" >= 2 )); do
+        dir=$1
+        new_remote_url=$2
+
+        if [ -d "$dir/.git" ]; then
+            echo "Checking remote URL in $dir..."
+            current_remote_url=$(git -C "$dir" remote get-url "$remote_name")
+            if [ "$current_remote_url" != "$new_remote_url" ]; then
+                echo "Updating remote URL in $dir from $current_remote_url to $new_remote_url..."
+                git -C "$dir" remote set-url "$remote_name" "$new_remote_url"
+                echo "Updated $dir"
+            else
+                echo "Skipping $dir: Remote URL is already set to $new_remote_url"
+            fi
+        else
+            echo "Skipping $dir: Not a git repository"
+        fi
+
+        # Shift past the current directory and URL
+        shift 2
+    done
+
+    echo "All specified repositories checked."
+}
+
+# Now update the repo remote URLs if needed
+update_git_remotes /var/www/dashboard https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-WebCode.git /usr/local/bin https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-Binaries.git /usr/local/sbin https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-Scripts.git
+#
+
 # check if user already has firewall disabled, and if so, ensure it's kept that way.
 if grep -q LOGNDROP /etc/iptables.rules; then
     fwState="enabled"
