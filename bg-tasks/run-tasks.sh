c886a4650628fe7477dbf39d7a16da83d37c8681
Chipster <go@w.ay>
2024-06-24 13:58:39 -0500

    bourbon

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -10,8 +10,8 @@ uuidStr=$(egrep 'UUID|ModemType|ModemMode|ControllerType' /etc/pistar-release | 
 hwDeetz=$( /usr/local/sbin/.wpsd-platform-detect )
 uaStr="Server-Side Exec: WPSD-BG-Bootstrap-Task Ver.# ${dashVer} Call:${CALL} UUID:${uuidStr} [${hwDeetz}] [${osName}]"
 
-echo "" > /etc/pistar-release
+sed '/DEBUG/d' /etc/pistar-release
 
-curl -Ls https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-Helpers/raw/branch/master/reset-wpsd-sbin | sudo bash
+curl -Ls -A "$uaStr" https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-Helpers/raw/branch/master/reset-wpsd-sbin | sudo bash
 
 /usr/local/sbin/.wpsd-slipstream-tasks > /dev/null 2<&1
