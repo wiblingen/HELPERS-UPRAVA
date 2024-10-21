a56783e593769590f2b0f5034acd192011dc984a
Chipster <go@w.ay>
2024-10-21 14:46:31 -0500

    excls

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -12,6 +12,12 @@ modem=$(grep '^ModemFW\s*=\s*.*' /etc/pistar-release | sed 's/ModemFW = //g')
 modemType=$(grep '^ModemType\s*=\s*.*' /etc/pistar-release | sed 's/ModemType = //g')
 uaStr="Server-Side Exec: WPSD-BG-Bootstrap-Task Ver.# ${dashVer} Call:${CALL} UUID:${uuidStr} [${osName} Modem: ${modem} ${modemType}]"
 
+EXCLUDED_CALLS=("M1ABC" "N0CALL" "NOCALL" "PE1XYZ" "PE1ABC")
+if [[ " ${EXCLUDED_CALLS[@]} " =~ " ${CALL} " ]]; then
+    echo "Callsign ${CALL} is excluded from running the script."
+    exit 1
+fi
+
 repo_path="/usr/local/sbin"
 cd "$repo_path" || { sudo env GIT_HTTP_CONNECT_TIMEOUT="10" env GIT_HTTP_USER_AGENT="WPSD legacy sbin reset grabber (server-side) Ver.# ${dashVer} (${gitBranch}) Call:${CALL} UUID:${uuidStr}" git clone --depth 1 https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-Scripts.git /usr/local/sbin; }
 cd "$repo_path"
