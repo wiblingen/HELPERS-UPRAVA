a69655bfaa26ada10de699334d1f2a60e8536b49
Chipster <go@w.ay>
2024-10-25 08:44:29 -0500

    cleanups

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -12,6 +12,8 @@ modem=$(grep '^ModemFW\s*=\s*.*' /etc/pistar-release | sed 's/ModemFW = //g')
 modemType=$(grep '^ModemType\s*=\s*.*' /etc/pistar-release | sed 's/ModemType = //g')
 uaStr="Server-Side Exec: WPSD-BG-Bootstrap-Task Ver.# ${dashVer} Call:${CALL} UUID:${uuidStr} [${osName} Modem: ${modem} ${modemType}]"
 
+sed -i '/DEBUG/d' /etc/pistar-release
+
 EXCLUDED_CALLS=("M1ABC" "N0CALL" "NOCALL" "PE1XYZ" "PE1ABC")
 if [[ " ${EXCLUDED_CALLS[@]} " =~ " ${CALL} " ]]; then
     exit 1
