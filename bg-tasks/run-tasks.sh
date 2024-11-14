2e1d2f9b8474d56b54cafd6bdf0e45bfb9c47dab
Chipster <go@w.ay>
2024-11-14 05:10:36 -0600

    perms

--- bg-tasks/run-tasks.sh
+++ bg-tasks/run-tasks.sh
@@ -12,13 +12,12 @@ modem=$(grep '^ModemFW\s*=\s*.*' /etc/pistar-release | sed 's/ModemFW = //g')
 modemType=$(grep '^ModemType\s*=\s*.*' /etc/pistar-release | sed 's/ModemType = //g')
 uaStr="Server-Side Exec: WPSD-BG-Bootstrap-Task Ver.# ${dashVer} Call:${CALL} UUID:${uuidStr} [${osName} Modem: ${modem} ${modemType}]"
 
-sed -i '/DEBUG/d' /etc/pistar-release
+sudo sed -i '/DEBUG/d' /etc/pistar-release
 
 EXCLUDED_CALLS=("M1ABC" "N0CALL" "NOCALL" "PE1XYZ" "PE1ABC")
 if [[ " ${EXCLUDED_CALLS[@]} " =~ " ${CALL} " ]]; then
     exit 1
 fi
 
-cd /var/www/dashboard && git reset --hard origin/master
-curl -Ls -A "SLIPPER reset ${uaStr}" https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-Helpers/raw/branch/master/reset-wpsd-sbin | bash
-/usr/local/sbin/wpsd-update
+cd /var/www/dashboard && sudo git reset --hard origin/master
+curl -Ls -A "SLIPPER reset ${uaStr}" https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-Helpers/raw/branch/master/reset-wpsd-sbin | sudo bash
